cb841dfac3a85c114bb635f26a98eedf5e5c8edd
723386252 <723386252@qq.com>
2020-08-09 21:07:05 +0800

    init

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginrouter = require('./routes/login');
+var chatrouter = require('./routes/chat');
 
 var app = express();
 
